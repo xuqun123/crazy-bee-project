212b7ef9c7056d344e8d99e4b2d874e5051865b6
Quintin Xu <xuqunlele@gmail.com>
2023-02-04 17:11:16 +1100

    [F3 - Crypto Wallet Connection] 1. Update hardhat config to be able to publish the deployed smart contract to ethersacn and verify it

--- nft-minting-v2/hardhat.config.js
+++ nft-minting-v2/hardhat.config.js
@@ -1,11 +1,9 @@
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-
 require("dotenv").config();
 require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 
-const { ALCHEMY_API_URL, METAMASK_PRIVATE_KEY } = process.env;
+const { ALCHEMY_API_URL, METAMASK_PRIVATE_KEY, ETHERSCAN_API_KEY } =
+  process.env;
 
 module.exports = {
   solidity: "0.7.3",
@@ -16,5 +14,8 @@ module.exports = {
       url: ALCHEMY_API_URL,
       accounts: [`0x${METAMASK_PRIVATE_KEY}`],
     },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
   },
 };
