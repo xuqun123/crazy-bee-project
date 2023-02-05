043542f24808852ed4f13f89bbd152b7f180e754
jamiecdavidson <117705808+jamiecdavidson@users.noreply.github.com>
2023-02-05 22:06:13 +1100

    [FE - Back Button] 1. addressed pull request changes

--- frontend/src/components/BackButton.js
+++ frontend/src/components/BackButton.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom'
-import Button from '@mui/material/Button'
+import IconButton from '@mui/material/IconButton'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack'
 
 const BackButton = () => {
   const location = useLocation()
@@ -9,10 +10,12 @@ const BackButton = () => {
     window.history.back()
   }
 
-  return location.pathname === '/' ? null : (
-    <Button variant="h3" color="inherit" fontWeight={'500'} noWrap onClick={handleClick}>
-      &laquo; Back
-    </Button>
+  return location.pathname === '/' ||
+    location.pathname === '/explore' ||
+    location.pathname === '/ai/creator' ? null : (
+    <IconButton variant="h5" color="inherit" fontWeight={'500'} noWrap onClick={handleClick}>
+      <ArrowBackIcon />
+    </IconButton>
   )
 }
 
